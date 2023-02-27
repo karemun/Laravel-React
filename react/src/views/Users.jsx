18cb42e459cef6d85e4dbc52a66c50e76f8b23cc
Karen Flores <karemu.8910@gmail.com>
2023-02-27 17:01:16 -0600

    Users CRUD api back-end

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,5 +1,28 @@
+import { useEffectm, useState } from "react";
+import axiosClient from "../axios-client";
+
 
 function Users() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    //Obtiene los usuarios despues del primer render
+    useEffect(()=>{
+        getUsers();
+    },[]);
+
+    const getUsers = () => {
+        setLoading(true);
+        axiosClient.get('/users')
+            .then(({data}) => {
+                setLoading(false);
+                console.log(data);
+            })
+            .catch(() => {
+                setLoading(false);
+            })
+    }
+
     return (
         <div>Users</div>
     )
